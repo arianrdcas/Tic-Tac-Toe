caece22d6ae20f74d9053912d2417edc262b7153
arianrdcas <arianrdcas@gmail.com>
2024-01-23 23:05:12 -0300

    Formulario de nombres ok

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,9 +3,24 @@ import { Fragment } from "react";
 import "./App.css";
 
 function App() {
+  /* const [jugador1, setJugador1] = useState("");
+  const [jugador2, setJugador2] = useState(""); */
 
-const[jugador1, setJugador1] = useState('')
-const [jugador2, setJugador2] = useState("");
+  const [jugadores, setJugadores] = useState({ jugador1: "", jugador2: "" });
+  const [nombresGuardados, setNombresGuardados] = useState(false);
+
+  const guardarNombres = (e) => {
+    e.preventDefault();
+    setNombresGuardados(true);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setJugadores((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   const Turn = {
     X: "x",
@@ -23,26 +38,34 @@ const [jugador2, setJugador2] = useState("");
       <div className="ventana">
         <div className="jugadores">
           <h1 className="nombre-juego">Tic Tac Toe</h1>
-          <div>
-            <p>Jugador 1:</p>
-            <input
-              type="text"
-              placeholder="Nombre jugador 1"
-              name="jugador1"
-              value={jugador1}
-              onChange={(e) => setJugador1(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Jugador 2:</p>
-            <input
-              type="text"
-              placeholder="Nombre jugador 2"
-              name="jugador2"
-              value={jugador2}
-              onChange={(e) => setJugador2(e.target.value)}
-            />
-          </div>
+          {nombresGuardados ? (
+            <div>
+              <h1>{jugadores.jugador1}</h1>
+              <h1>{jugadores.jugador2}</h1>
+            </div>
+          ) : (
+            <form onSubmit={guardarNombres}>
+              <p>Jugador 1:</p>
+              <input
+                type="text"
+                placeholder="Nombre jugador 1"
+                name="jugador1"
+                value={jugadores.jugador1}
+                onChange={handleInputChange}
+              />
+              <p style={{ marginTop: "10px" }}>Jugador 2:</p>
+              <input
+                type="text"
+                placeholder="Nombre jugador 2"
+                name="jugador2"
+                value={jugadores.jugador2}
+                onChange={handleInputChange}
+              />
+              <button className="boton-guardar" type="submit">
+                Guardar nombres
+              </button>
+            </form>
+          )}
         </div>
         <div className="square">
           {board.map((_, index) => {
