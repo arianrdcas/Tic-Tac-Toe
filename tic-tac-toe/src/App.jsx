6fb203025f835ef75903c44db52185f480936069
arianrdcas <arianrdcas@gmail.com>
2024-01-25 19:05:24 -0300

    Funcion del juego terminado

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -7,20 +7,35 @@ const Turns = {
   O: "O",
 };
 
+const Cuadricula = ({ children, isSelected, actualizarCuadricula, index }) => {
+  const className = `cuadricula ${isSelected ? "is-selected" : ""}`;
 
+  const handleClick = () => {
+    actualizarCuadricula(index);
+  };
 
-const Cuadricula = ({ children, updateCuadricula, index }) => {
-  return <div className="cuadricula">{children}</div>;
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
-
+const comboGanador = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
-
   const [jugadores, setJugadores] = useState({ jugador1: "", jugador2: "" });
   const [nombresGuardados, setNombresGuardados] = useState(false);
-  const [turn, setTurn] = useState(Turns.X)
-
+  const [turn, setTurn] = useState(Turns.X);
 
   const guardarNombres = (e) => {
     e.preventDefault();
@@ -36,18 +51,57 @@ function App() {
   };
 
   const [board, setBoard] = useState(Array(9).fill(null));
-  
+  const [ganador, setGanador] = useState(null);
+
+  const revisarGanador = (cuadriculaCheck) => {
+    for (const combo of comboGanador) {
+      const [a, b, c] = combo;
+      if (
+        cuadriculaCheck[a] &&
+        cuadriculaCheck[a] === cuadriculaCheck[b] &&
+        cuadriculaCheck[a] === cuadriculaCheck[c]
+      ) {
+        return cuadriculaCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const reiniciarJuego = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(Turns.X);
+    setGanador(null);
+  };
+
+  const checkEndGame = (nuevoBoard) => {
+    return nuevoBoard.every((cuadricula) => cuadricula !== null);
+  };
+
+  const actualizarCuadricula = (index) => {
+    if (board[index] || ganador) return;
+    const nuevoBoard = [...board];
+    nuevoBoard[index] = turn;
+    setBoard(nuevoBoard);
+    const nuevoTurno = turn === Turns.X ? Turns.O : Turns.X;
+    setTurn(nuevoTurno);
+    const nuevoGanador = revisarGanador(nuevoBoard);
+    if (nuevoGanador) {
+      setGanador(nuevoGanador);
+    } else if (checkEndGame(nuevoBoard)) {
+      setGanador(false);
+    }
+  };
 
   return (
     <Fragment>
       <div className="ventana">
-        <div className="jugadores">
-          <h1 className="nombre-juego">Tic Tac Toe</h1>
+        <h1 className="nombre-juego">Tic Tac Toe</h1>
+        {/* <div className="jugadores">     
           {nombresGuardados ? (
             <div>
-              <h2>Jugador 1 :</h2>
+              <h2>Jugador 1 </h2>
               <h3 className="jugador1">{jugadores.jugador1}</h3>
-              <h2>Jugador 2 :</h2>
+              <h2>Jugador 2 </h2>
               <h3>{jugadores.jugador2}</h3>
             </div>
           ) : (
@@ -71,16 +125,32 @@ function App() {
               </button>
             </form>
           )}
-        </div>
+        </div> */}
+        <section>
+          <Cuadricula isSelected={turn === Turns.X}>{Turns.X}</Cuadricula>
+          <Cuadricula isSelected={turn === Turns.O}>{Turns.O}</Cuadricula>
+        </section>
         <div className="square">
           {board.map((_, index) => {
-            return <Cuadricula key={index}>{board[index]}</Cuadricula>;
+            return (
+              <Cuadricula
+                key={index}
+                index={index}
+                actualizarCuadricula={actualizarCuadricula}
+              >
+                {board[index]}
+              </Cuadricula>
+            );
           })}
         </div>
-        <section id="turno-jugador">
-          <Cuadricula isSelected={turn === Turns.X}>{Turns.X}</Cuadricula>
-          <Cuadricula>{Turns.O}</Cuadricula>
+        <section>
+          {ganador !== null && (
+            <h2>{ganador === false ? "Empate" : "Gano " + ganador}</h2>
+          )}
         </section>
+        <footer>
+          <button onClick={reiniciarJuego}>Reiniciar juego</button>
+        </footer>
       </div>
     </Fragment>
   );
